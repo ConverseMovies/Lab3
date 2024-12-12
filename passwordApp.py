0c19ef65c5a4ae1c2a0f09521dd242715338114b
ConverseMovies <gegger@uiowa.edu>
2024-12-12 10:11:30 -0600

    add new files for heroku

--- passwordApp.py
+++ passwordApp.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -49,4 +50,4 @@ def gavin():
     return render_template('gavin.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
